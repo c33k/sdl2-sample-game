afc2c7599dfdf5bba8221a22e5aa658ec216acee
c33k <rltoscano@gmail.com>
2019-10-04 18:55:23 +0200

    Initial Commit

---
+++ SDL_TEST_YOUTUBE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_TEST_YOUTUBE
+//
+//  Created by Rafael L. Toscano on 04/10/19.
+//  Copyright © 2019 c33k. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
